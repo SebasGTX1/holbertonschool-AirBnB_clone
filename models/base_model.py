8917411441a14c8e48170d3cf26cf134c1361e43
SebasGTX1 <sebas-0202@hotmail.es>
2022-07-01 11:03:37 -0500

    Completing the to_dict method, Fixing pycodestyle and adding the default values for id and created_at

--- models/base_model.py
+++ models/base_model.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
 """
-Write a class BaseModel that defines all common 
+Write a class BaseModel that defines all common
 attributes/methods for other classes
 """
-
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """Class Base Model"""
-    def __init__(self, id, created_at):
+    def __init__(self, id=0, created_at=0):
         """Initialization"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        """should print: [<class name>] (<self.id>) <self.__dict__>""" 
+        """should print: [<class name>] (<self.id>) <self.__dict__>"""
         return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
-        """updates the public instance attribute updated_at with the current datetime"""
+        """updates the public instance attribute
+            updated_at with the current datetime
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance"""
-        pass
-
+        """returns a dictionary containing all keys/values
+        of __dict__ of the instance
+        """
+        self.__dict__["__class__"] = self.__class__.__name__
+        return self.__dict__
