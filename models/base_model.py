153cee3171864136294cb6e3232674059a1eaaac
SebasGTX1 <sebas-0202@hotmail.es>
2022-07-01 14:50:09 -0500

    Fixing creation of date attributes and to_dict method

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,21 @@ from datetime import datetime
 
 class BaseModel:
     """Class Base Model"""
-    def __init__(self, id=0, created_at=0, updated_at=0):
+    def __init__(self, *args, **kwargs):
         """Initialization"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.isoformat(datetime.now())
-        self.updated_at = datetime.isoformat(datetime.now())
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    date_format = "%Y-%m-%dT%H:%M:%S.%f"
+                    date_time_obj = datetime.strptime(value, date_format)
+                    setattr(self, key, date_time_obj)
+                    continue
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """should print: [<class name>] (<self.id>) <self.__dict__>"""
@@ -23,11 +33,19 @@ class BaseModel:
         """updates the public instance attribute
             updated_at with the current datetime
         """
-        self.updated_at = datetime.isoformat(datetime.now())
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values
         of __dict__ of the instance
         """
-        self.__dict__["__class__"] = self.__class__.__name__
-        return self.__dict__
+        my_dict = {}
+
+        for key, value in self.__dict__.items():
+            if key == "created_at" or key == "updated_at":
+                my_dict[key] = value.isoformat()
+                continue
+            if value:
+                my_dict[key] = value
+        my_dict['__class__'] = self.__class__.__name__
+        return my_dict
