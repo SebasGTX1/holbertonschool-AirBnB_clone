117e8a3a234b1d5552f64c488c9537739f95c5b7
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-06-30 23:18:58 -0500

    add save to test

--- models/base_model.py
+++ models/base_model.py
@@ -18,3 +18,12 @@ class BaseModel:
     def __str__(self):
         """should print: [<class name>] (<self.id>) <self.__dict__>""" 
         return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+
+    def save(self):
+        """updates the public instance attribute updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        pass
+
