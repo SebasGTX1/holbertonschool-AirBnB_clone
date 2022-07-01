4d6a998dbcad986ea9d30c3577733d7fe9f82853
SebasGTX1 <sebas-0202@hotmail.es>
2022-07-01 11:06:44 -0500

    Fixing ISOformat in dates and adding default value for update attributte

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,11 @@ from datetime import datetime
 
 class BaseModel:
     """Class Base Model"""
-    def __init__(self, id=0, created_at=0):
+    def __init__(self, id=0, created_at=0, updated_at=0):
         """Initialization"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.isoformat(datetime.now())
+        self.updated_at = datetime.isoformat(datetime.now())
 
     def __str__(self):
         """should print: [<class name>] (<self.id>) <self.__dict__>"""
@@ -23,7 +23,7 @@ class BaseModel:
         """updates the public instance attribute
             updated_at with the current datetime
         """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.isoformat(datetime.now())
 
     def to_dict(self):
         """returns a dictionary containing all keys/values
