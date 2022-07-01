7542a892cad174834c5c313bbe553edaa86aa6c2
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-06-30 23:08:17 -0500

    just some to test

--- models/base_model.py
+++ models/base_model.py
@@ -3,5 +3,18 @@
 Write a class BaseModel that defines all common 
 attributes/methods for other classes
 """
+
+import uuid
+from datetime import datetime
+
 class BaseModel:
-    pass
+    """Class Base Model"""
+    def __init__(self, id, created_at):
+        """Initialization"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """should print: [<class name>] (<self.id>) <self.__dict__>""" 
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
