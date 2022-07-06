9faa155121d9440120091dbf35717eab49ff32ab
SebasGTX1 <sebas-0202@hotmail.es>
2022-07-06 10:21:10 -0500

    Fixing tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -25,7 +25,7 @@ class FileStorageTests(unittest.TestCase):
         all_objs = storage.all()
 
         key = basemodel_dict['__class__'] + "." + basemodel_dict['id']
-        self.assertEqual(key in all_objs, False)
+        self.assertEqual(key in all_objs, True)
 
     def testStoreBaseModel2(self):
         """ Test save, reload and update functions """
@@ -35,7 +35,7 @@ class FileStorageTests(unittest.TestCase):
 
         key = basemodel_dict['__class__'] + "." + basemodel_dict['id']
 
-        self.assertEqual(key in storage.all(), False)
+        self.assertEqual(key in storage.all(), True)
         self.assertEqual(basemodel_dict['my_name'], "First name")
 
         create1 = basemodel_dict['created_at']
@@ -45,7 +45,7 @@ class FileStorageTests(unittest.TestCase):
         self.basemodel.save()
         basemodel_dict = self.basemodel.to_dict()
 
-        self.assertEqual(key in storage.all(), False)
+        self.assertEqual(key in storage.all(), True)
 
         create2 = basemodel_dict['created_at']
         update2 = basemodel_dict['updated_at']
