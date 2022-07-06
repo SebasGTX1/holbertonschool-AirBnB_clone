51434c2ffb5fcef0880ebff306135cadebea60f9
SebasGTX1 <sebas-0202@hotmail.es>
2022-07-06 11:00:18 -0500

    Update test

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+""" Test file for User class """
+import unittest
+from numpy import place
+from models import amenity
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models import storage
+from models.engine.file_storage import FileStorage
+import datetime
+
+
+class TestAmenity(unittest.TestCase):
+    def setUp(self):
+        """
+        the setUp() method raises an exception
+        while the test is running
+        """
+        pass
+
+    def tearDown(self):
+        """
+        a tearDown() method that tidies up after
+        the test method has been run
+        """
+        pass
+
+    def test_amenity_instantiation(self):
+        """Amenity instance attributes"""
+        amenity = Amenity()
+        s = "<class 'models.amenity.Amenity'>"
+        self.assertEqual(str(type(amenity)), s)
+        self.assertIsInstance(amenity, Amenity)
+        self.assertTrue(issubclass(type(amenity), BaseModel))
+
+    def test_amenity_Attributes(self):
+        """Test Attributes exist"""
+        amenity = Amenity()
+        self.assertTrue(hasattr(amenity, 'name'))
+        self.assertTrue(hasattr(amenity, 'id'))
+        self.assertTrue(hasattr(amenity, 'created_at'))
+        self.assertTrue(hasattr(amenity, 'updated_at'))
+
+    def test_amenity_types(self):
+        """Amenity correct attributes' types"""
+        amenity = Amenity()
+        self.assertIsInstance(amenity.name, str)
+        self.assertIsInstance(amenity.id, str)
+        self.assertIsInstance(amenity.created_at, datetime.datetime)
+        self.assertIsInstance(amenity.updated_at, datetime.datetime)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_models/test_user.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+""" Test file for User class """
+import unittest
+from models.base_model import BaseModel
+from models.user import User
+from models import storage
+from models.engine.file_storage import FileStorage
+import datetime
+
+
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        """
+        the setUp() method raises an exception
+        while the test is running
+        """
+        pass
+
+    def tearDown(self):
+        """
+        a tearDown() method that tidies up after
+        the test method has been run
+        """
+        pass
+
+    def test_user_instantiation(self):
+        """User instance attributes"""
+        user = User()
+        self.assertEqual(str(type(user)), "<class 'models.user.User'>")
+        self.assertIsInstance(user, User)
+        self.assertTrue(issubclass(type(user), BaseModel))
+
+    def test_user_Attributes(self):
+        """User Attributes exist"""
+        user = User()
+        self.assertTrue(hasattr(user, 'email'))
+        self.assertTrue(hasattr(user, 'password'))
+        self.assertTrue(hasattr(user, 'first_name'))
+        self.assertTrue(hasattr(user, 'last_name'))
+        self.assertTrue(hasattr(user, 'id'))
+        self.assertTrue(hasattr(user, 'created_at'))
+        self.assertTrue(hasattr(user, 'updated_at'))
+
+    def test_user_types(self):
+        """User correct attributes' types"""
+        user = User()
+        self.assertIsInstance(user.first_name, str)
+        self.assertIsInstance(user.last_name, str)
+        self.assertIsInstance(user.email, str)
+        self.assertIsInstance(user.password, str)
+        self.assertIsInstance(user.id, str)
+        self.assertIsInstance(user.created_at, datetime.datetime)
+        self.assertIsInstance(user.updated_at, datetime.datetime)
+
+
+if __name__ == '__main__':
+    unittest.main()
